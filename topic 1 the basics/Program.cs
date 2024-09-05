8dd4021815cfebc7097eedc01d1c5f38a845b145
362568651 <362568651@2260I-L29-029>
2024-09-05 09:29:13 -0400

    Add project files.

---
+++ topic 1 the basics/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace topic_1_the_basics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Jonathan
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("i will print on a new line");
+            Console.WriteLine("press ENTER to continue");
+            Console.ReadLine();
+            Console.WriteLine();
+            Console.Write("hello world,");
+            Console.Write(" what is different?");
+        }
+    }
+}
